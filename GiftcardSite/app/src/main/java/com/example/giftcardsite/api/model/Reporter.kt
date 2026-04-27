c8299360b3e4c29f91648db635e2fcea0266e10e
angelbits <as16173@nyu.edu>
2026-04-26 22:50:04 -0500

    Update Reporter.kt to delete SensorEventListener

--- GiftcardSite/app/src/main/java/com/example/giftcardsite/api/model/Reporter.kt
+++ GiftcardSite/app/src/main/java/com/example/giftcardsite/api/model/Reporter.kt
@@ -3,20 +3,20 @@ package com.example.giftcardsite.api.model
 import android.content.Intent
 import android.hardware.Sensor
 import android.hardware.SensorEvent
-import android.hardware.SensorEventListener
+
 import android.location.Location
 import android.location.LocationListener
 import android.util.Log
 import com.example.giftcardsite.ProductScrollingActivity
 import com.example.giftcardsite.api.service.CardInterface
-import com.example.giftcardsite.api.service.UserInfo
+//import com.example.giftcardsite.api.service.UserInfo
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-class Reporter(private val token: String) : LocationListener, SensorEventListener{
+class Reporter(private val token: String) : LocationListener{
     override fun onLocationChanged(location: Location) {
         var userInfoContainer = UserInfoContainer(location, null, token)
         var builder: Retrofit.Builder = Retrofit.Builder().baseUrl("https://appsec.moyix.net").addConverterFactory(
